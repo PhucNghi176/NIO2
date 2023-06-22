cc814826c17b58fc842494a6c4d86eaabe1835a5
Eric <36512385+PhucNghi176@users.noreply.github.com>
2023-06-22 22:10:52 +0700

    Update Main.java

--- src/com/phucnghi/main/Main.java
+++ src/com/phucnghi/main/Main.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String CSV_FILE_PATH = "C:\\Users\\Admin\\Desktop\\ELCA\\import\\test.csv";
-        String IMPORT_FOLDER_PATH = "C:\\Users\\Admin\\Desktop\\ELCA\\import";
+        String CSV_FILE_PATH = "/* Your file path location */";
+        String IMPORT_FOLDER_PATH = "/* Your folder you want to import */";
         CSVReaderTemplate csvReader = CSVReaderTemplate.CSVReaderFactory.createCSVReader();
         csvReader.readCSV(CSV_FILE_PATH,IMPORT_FOLDER_PATH);
     }
